300ef1a55fecc3a28bf6635b0960a99aa99a4714
tran <c12simple@gmail.com>
2019-06-19 13:47:50 +0200

    restructure ldap

--- scripts/generate-users-ldif.php
+++ scripts/generate-users-ldif.php
@@ -27,7 +27,11 @@ if (($handle = fopen($source_file_path, "r")) !== FALSE) {
         echo "objectClass: posixAccount\n";
         echo "uidNumber: ".$data[0]."\n";
         echo "uid: ".$data[2]."\n";
-        echo "homeDirectory: /home/".$data[2]."\n";
+        if ($data[3] != "Umlaut"){
+            echo "homeDirectory: /home/".$data[2]."\n";
+        }else{
+            echo "homeDirectory: /home/".$data[3]."\n";
+        }
         echo "sn: ".$data[3]."\n";
         echo "cn: ".$data[4]." ".$data[3]."\n";
         echo "mail: ".$data[1]."\n";
@@ -35,6 +39,21 @@ if (($handle = fopen($source_file_path, "r")) !== FALSE) {
         echo "displayName: ".$data[4]." ".$data[3]."\n";
         echo "userPassword:: UEBzc3cwcmQ=\n";
         echo "\n";
+
+        if ($data[6] != ""){
+            $groups = explode("|", $data[6]);
+            foreach($groups as $group)
+            {
+                if (!empty($group)){
+                    echo "dn: ".$group."\n";
+                    echo "changetype: modify\n";
+                    echo "add: member\n";
+                    echo "member: uid=".$data[2].",ou=".$data[5].",ou=people,".$baseDN."\n";
+                    echo "\n";
+                }
+            }
+        }
+
         if (($row < 1020) && ($data[5] !== "visitor")) {
             $members .= "member: uid=".$data[2].",ou=".$data[5].",ou=people,".$baseDN."\n";
         }
@@ -43,11 +62,11 @@ if (($handle = fopen($source_file_path, "r")) !== FALSE) {
 
     fclose($handle);
 
-    echo "dn: cn=employees,ou=groups,".$baseDN."\n";
+    echo "dn: cn=alluser,ou=groups,".$baseDN."\n";
     echo "changetype: add\n";
     echo "objectClass: groupOfNames\n";
     echo "objectClass: top\n";
-    echo "cn: employees\n";
+    echo "cn: alluser\n";
     echo $members;
     echo "description: All employees of the Example organisation\n";
 }
